3509e29a1c3a62dda633560ab6d168acec8d941a
YanluPei <peiyanlu@vip.qq.com>
2026-04-14 22:18:42 +0800

    feat: 测试

--- publish.js
+++
@@ -1,8 +0,0 @@
-import { publishTagToNpm } from '@peiyanlu/release'
-
-
-await publishTagToNpm({
-  gitTag: '1.0.16',
-  getPkgDir: () => '.',
-  defaultPackage: 'test-release-01'
-})
---
+++ publishCI.ts
@@ -0,0 +1,4 @@
+import { publishTagToNpm } from '@peiyanlu/release'
+
+
+await publishTagToNpm({})
